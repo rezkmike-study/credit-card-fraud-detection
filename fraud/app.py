725decd4b0ebc97306f1f20d59049391ac1ddd11
Kamil <kamil.kamaruddin@paynet.my>
2024-01-11 09:09:07 +0800

    Update: training column

--- fraud/app.py
+++ fraud/app.py
@@ -187,7 +187,7 @@ def main():
     st.markdown("""
         <style>
         .main {
-            background-color: #F5F5F5;
+            background-color: #171716;
         }
         .description {
             border: 1px solid #4F8BF9;
