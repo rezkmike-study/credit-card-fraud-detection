f14bbc0bc3d4872f30c7aa37c09064f02f0db74e
Kamil <kamil.kamaruddin@paynet.my>
2024-01-11 09:12:05 +0800

    Update: training column

--- fraud/app.py
+++ fraud/app.py
@@ -78,12 +78,6 @@ def user_input_features():
 
     # Combine the features into a dataframe
     data = {
-        'amt': amt,
-        'lat': lat,
-        'long': long,
-        'merch_lat': merch_lat,
-        'merch_long': merch_long,
-        'city_pop': city_pop,
         'category_shopping_pos': 1 if category == 'shopping_pos' else 0,
         'category_entertainment': 1 if category == 'entertainment' else 0,
         'category_gas_transport': 1 if category == 'gas_transport' else 0,
@@ -92,6 +86,7 @@ def user_input_features():
         'category_misc_net': 1 if category == 'misc_net' else 0,
         'category_misc_pos': 1 if category == 'misc_pos' else 0,
         'category_shopping_net': 1 if category == 'shopping_net' else 0,
+        'amt': amt,
         'city_Orient': 1 if city == 'Orient' else 0,
         'city_Malad-City': 1 if city == 'Malad-City' else 0,
         'city_Grenada': 1 if city == 'Grenada' else 0,
@@ -173,7 +168,12 @@ def user_input_features():
         'state_AZ': 1 if state == 'AZ' else 0,
         'state_CO': 1 if state == 'CO' else 0,
         'state_MO': 1 if state == 'MO' else 0,
-        'state_AK': 1 if state == 'AK' else 0
+        'state_AK': 1 if state == 'AK' else 0,
+        'lat': lat,
+        'long': long,
+        'city_pop': city_pop,
+        'merch_lat': merch_lat,
+        'merch_long': merch_long
     }
 
     features = pd.DataFrame(data, index=[0])
